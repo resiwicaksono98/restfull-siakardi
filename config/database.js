4a73171e98ad8dc96289de5a8d9749c043962e71
Resi Wicaksono <resiwicaksono98@gmail.com>
2022-12-02 21:41:36 +0700

    change database setting

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 import { dbName, dbHost, dbUser, dbPass, dbPort } from "./config.js";
 
 const db = new Sequelize(dbName, dbUser, dbPass, {
-  host: "127.0.0.1",
+  host: dbHost,
   dialect: "mysql",
 });
 
